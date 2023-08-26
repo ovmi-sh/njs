459b9e34c3779ac08c38f76875863c723458ba14
micovi <micleaovidiul@gmail.com>
2023-08-26 14:07:07 +0300

    feat: update logo component and usage

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -44,7 +44,7 @@ export default function SignUpPage() {
         </Link>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
+          <div className="relative z-20 flex items-center gap-2 text-lg font-medium">
             <Logo />
           </div>
           <div className="relative z-20 mt-auto">
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,19 +1,13 @@
+import { siteConfig } from "@/config/site";
+import { Icons } from "./icons";
+
 export default function Logo() {
   return (
     <>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="mr-2 h-6 w-6"
-      >
-        <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-      </svg>
-      Logo
+      <Icons.logo />
+      <span className="hidden font-bold sm:inline-block">
+        {siteConfig.name}
+      </span>
     </>
   );
 }
